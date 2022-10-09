c3f4894cfa76ccd8915cbbefdba340ead288071f
Gauthier Lambeau <gauthier.lambeau@student.ehb.be>
2022-10-09 11:17:13 +0200

    git hub logo (space invader)

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,68 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawLine();
+
+function drawLine() {
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+context.lineWidth = 3;
+context.fillStyle = "black";
+context.beginPath();
+context.rect(0, 0, 300, 300);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(125, 25, 50, 50);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(125, 125, 50, 50);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(75, 175, 50, 50);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(175, 175, 50, 50);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(25, 75, 50, 100);
+context.stroke();
+context.fill();
+context.stroke();
+
+context.lineWidth = 3;
+context.fillStyle = "limegreen";
+context.beginPath();
+context.rect(225, 75, 50, 100);
+context.stroke();
+context.fill();
+context.stroke();
+}
+
